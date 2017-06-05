ede3680b7350e5a5cf21ba21c40a6f3eb3835097
satyam0078 <satyamshekhar9535@gmail.com>
2017-06-05 23:38:30 +0530

    first commit for now ..soon more

---
+++ src/hwgdcclient/HWGDCClient.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hwgdcclient;
+
+import StartingWindow.StartingWindow;
+
+/**
+ *
+ * @author satyam
+ */
+public class HWGDCClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StartingWindow sw=new StartingWindow();
+        sw.setVisible(true);
+        
+        // TODO code application logic here
+    }
+    
+}
